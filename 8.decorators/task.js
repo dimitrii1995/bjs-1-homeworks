0f0e3c2ddae0116f13e5c224cdab550d2047c142
dimitrii1995 <136495842+dimitrii1995@users.noreply.github.com>
2023-07-02 12:02:48 +0300

    8 задание

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,73 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+	let cache = [];
+
+	function wrapper(...args) {
+	const hash = md5(JSON.stringify(args));
+	let objectInCache = cache.find((item) => item.hash === hash);
+
+	if (objectInCache) {
+		console.log("Из кэша: " + objectInCache.value);
+		return "Из кэша: " + objectInCache.value;
+		}
+
+	let result = func(...args);
+		cache.push({
+			hash: hash,
+			value: result
+		});
+
+	if (cache.length > 5) {
+			cache.shift();
+		}
+
+	console.log("Вычисляем: " + result);
+		return "Вычисляем: " + result;
+	}
+
+	return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+	let timeoutId = null;
+	let count = 0;
+	let allCount = 0;
+
+	function decoratFunction(...args) {
+	if (timeoutId) {
+			clearTimeout(timeoutId);
+			timeoutId = null;
+		}
+
+	if (count === 0) {
+		console.log(func(...args));
+		count++;
+		allCount++;
+		} else {
+		allCount++;
+		}
+
+	timeoutId = setTimeout(() => {
+		timeoutId = null;
+		console.log(func(...args));
+		count++;
+		}, delay);
+	}
+
+	Object.defineProperty(decoratFunction, "count", {
+		get: function() {
+		return count;
+		},
+	});
+
+	Object.defineProperty(decoratFunction, "allCount", {
+		get: function() {
+		return allCount;
+		},
+	});
+
+	decoratFunction.history = [];
+
+	return decoratFunction;
 }
